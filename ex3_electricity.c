fb86c6f13ab1c66f4c3f05b7f25ed337a5009802
namuganzamariagorret-tech <namuganzamariagorret@gmail.com>
2026-03-24 20:53:54 +0300

    completed assignment 1

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,20 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Namuganza Maria Gorret */
+/* Student Number: 25/U/BIO/027/GV*/
 
 #include <stdio.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main(void) {
+    float unitsConsumed, unitPrice;
+    
+    printf("Enter units consumed:%.2f\n", unitsConsumed);
+    scanf("%f", &unitsConsumed);// reading the units consumed from the user and storing it in the variable unitsConsumed
+    
+    printf("Enter cost per unit:%.2f\n", unitPrice);
+    scanf("%f", &unitPrice);// reading the cost per unit from the user and storing it in the variable unitPrice
+    
+    float totalBill = unitsConsumed*unitPrice;// calculating total cost
+    
+    printf("Total Bill:%.2f\n", totalBill);
 
     return 0;
 }
