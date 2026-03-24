a82143725c0731d59ea37f60c973c71a180f3ffa
namuganzamariagorret-tech <namuganzamariagorret@gmail.com>
2026-03-24 20:51:57 +0300

    completed assignment 1

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,21 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Namuganza Maria Gorret */
+/* Student Number: 25/U/BIO/027/GV*/
 
 #include <stdio.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main(void) {
+    float distanceTravelled;
+    printf("Enter distance(km):%.2f\n", distanceTravelled);
+    scanf("%f", &distanceTravelled);// reading the distance travelled from the user and storing in the variable distanceTravelled
+
+    float fuelUsed;
+    printf("Enter fuel used(liters):%.2f\n", fuelUsed);
+    scanf("%f", &fuelUsed);
+
+    float fuelEfficiency;
+    fuelEfficiency = distanceTravelled/fuelUsed;// calculating fuel efficiency 
+
+    printf("Fuel efficiency: %.2f\n", fuelEfficiency);// printing result
 
     return 0;
 }
