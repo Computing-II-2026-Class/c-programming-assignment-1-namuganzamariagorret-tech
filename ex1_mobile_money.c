1be8ba3e0f1f032e450d5a4bf98a140e2537ce4c
namuganzamariagorret-tech <namuganzamariagorret@gmail.com>
2026-03-24 20:49:54 +0300

    completed assignment 1

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,24 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Namuganza Maria Gorret */
+/* Student Number: 25/U/BIO/027/GV*/
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float amountToSend;
+    printf("Enter amount to send: %f\n", amountToSend);
+    scanf("%f", &amountToSend);// reading the fee to be sent and storing in the varaible amountToSend
+    float transactionFee;
+    printf("Enter transaction fee(%): %.2f\n", transactionFee);
+    scanf("%f", &transactionFee);// reading the transaction fee and storing it in the variable tansactionFee
+    
+    float feeAmount, totalDeducted;
+    
+    feeAmount = (transactionFee/100)*amountToSend;
+    totalDeducted = amountToSend + feeAmount;
+    
+    printf("Transaction fee:%.2f\n", feeAmount);
+    printf("Total deducted: %.2f\n", totalDeducted);
 
     return 0;
 }
